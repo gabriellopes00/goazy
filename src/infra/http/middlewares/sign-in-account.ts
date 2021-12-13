75b558c51903e4e5945bf5494b7ca79e45687596
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-12-13 10:55:08 -0300

    feat: create sign-in account middleware

---
+++ src/infra/http/middlewares/sign-in-account.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express'
+import { verify } from 'jsonwebtoken'
+
+export async function signInAccount(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<Response> {
+  const token = req.headers.authorization
+  if (!token) return res.status(401).json({ error: 'Missing auth token' })
+
+  const payload = token.split(' ')[1]
+
+  try {
+    verify(payload, '3EEa09sdf05a7iB7sIGdf90i') as { id: string }
+  } catch (error) {
+    return res.status(401).json({ error: 'Invalid auth token' })
+  }
+}
